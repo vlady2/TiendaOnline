e3e6c633e4505e6b5894a9b44713ded2293492f6
vlady2 <vladimirvides1@gmail.com>
2022-06-27 11:19:52 -0600

    detalles de navegacion

--- model.js
+++ model.js
@@ -1,7 +1,5 @@
 function detalleProduct(nombre, srcImage){
-    //imgPortada
     document.getElementById("imgDetalle").src = srcImage;
     document.getElementById("pDetalle").innerHTML = nombre;
     document.getElementById("pnProductDetail").style.visibility = '';
-
 }
